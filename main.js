7e789974e65a55fffa496d049b58ddf9af0bfe26
nadiawaseem <161966596+Nadiawaseem@users.noreply.github.com>
2024-06-08 17:10:53 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+"use strict";
+let age = 60;
+if (age < 2) {
+    console.log("the person is a baby.");
+}
+else if (age >= 2 && age < 4) {
+    console.log("the person is a toddler.");
+}
+else if (age >= 4 && age < 13) {
+    console.log("the person is kid.");
+}
+else if (age >= 13 && age < 20) {
+    console.log("the person is teenager.");
+}
+else if (age >= 20 && age < 65) {
+    console.log("the person is adult.");
+}
+else if (age >= 65 && age > 65) {
+    console.log("the person is a elder.");
+}
